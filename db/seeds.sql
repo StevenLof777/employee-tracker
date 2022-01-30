eac8eabda2589e9743b8fa25a5628e9b4f707e30
Jake Finn <lof.steven@gmail.com>
2022-01-30 17:17:13 -0600

    Make seeds and update notes.md

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO roles (id, title, salary)
+VALUES (001, "Software Engineer", 70000),
+       (002, "Lead Engineer", 120000),
+       (003, "Accountant", 70000);
+
+INSERT INTO department (id, department)
+VALUES (001, 'Engineering'),
+       (002, 'Finance'),
+       (003, 'Legal');
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES (001, 'Engineering'),
+       (002, 'Finance'),
+       (003, 'Legal');

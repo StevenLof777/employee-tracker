f836c8f9871f9c19d0ff84f359d30aab487af0c0
Jake Finn <lof.steven@gmail.com>
2022-01-31 21:52:11 -0600

    Fix add department prompt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,17 @@
 -- department
-INSERT INTO department (id, name)
+INSERT INTO department (id, department)
 VALUES (001, 'Engineering'),
        (002, 'Finance'),
        (003, 'Legal');
  
 -- roles 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (001, "Software Engineer", 70000, 1),
-       (002, "Lead Engineer", 120000, 2),
-       (003, "Accountant", 70000, 3);
+INSERT INTO roles (id, department_id, title, salary)
+VALUES (001, 001, "Software Engineer", 70000),
+       (002, 001, "Lead Engineer", 120000),
+       (003, 003, "Accountant", 70000);
  
 -- employee 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (001, 'John', 'Wick', 002),
+VALUES (001, 'John', 'Wick', 002, NULL),
        (002, 'Joe', 'Schmoe', 001, 1),
        (003, 'Emily', 'Simile', 003, 2);
- 
